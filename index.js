45794ff9274ab65fd790969deb2d5647f3dd24f1
cuongcm22 <cuongcm202@gmail.com>
2025-03-28 16:51:51 +0700

    Update PromtAppv1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const navItems = [
     { title: "Trang chủ", link: "/", target: "_blank" },
     { title: "Contact", link: "#contact", target: "_blank" },
     { title: "Blog", link: "#blog", target: "_blank" },
-    { title: "Support", link: "#support", target: "_blank" },
+    { title: "PromtApp-v1", link: "/PromtAppv1/index.html", target: "_blank" },
     { title: "Support", link: "#support", target: "_blank" },
     { title: "Support", link: "#support", target: "_blank" },
     { title: "Support", link: "#support", target: "_blank" },
