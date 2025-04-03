cb36aae0c91aacfc54eb0efab70de2a390c4c979
danhnam1315 <danhnam1315@gmail.com>
2025-04-03 23:15:27 +0700

    09-TextSummary-Nam

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const navItems = [
     { title: "02-Giải nghĩa nhanh từ khóa", link: "/PromtAppv1/02-GiaiNghiaNhanhTuKhoa.html", target: "_blank" },
     { title: "03-Dịch thuật sát nghĩa", link: "/PromtAppv1/03-DichThuatSatNghia.html", target: "_blank" },
     { title: "04-Prompt Translator App", link: "/PromtAppv1/04-PromptTranslatorApp.html", target: "_blank" },
+    { title: "05-Text Summary App", link: "/PromtAppv1/05-TextSummary.html", target: "_blank" },
     { title: "Support", link: "#support", target: "_blank" },
     { title: "Support", link: "#support", target: "_blank" },
     { title: "Support", link: "#support", target: "_blank" },
